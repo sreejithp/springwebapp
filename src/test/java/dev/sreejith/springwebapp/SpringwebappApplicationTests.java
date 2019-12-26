ba7c8fd38754e2053ac718db53e40927f7187d50
Sreejith <sreejith@ridezum.com>
2019-12-25 23:40:45 -0800

    Boiler plate

---
+++ src/test/java/dev/sreejith/springwebapp/SpringwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sreejith.springwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
